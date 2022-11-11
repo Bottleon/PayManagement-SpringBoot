79b136b75f4dcbc88cca21ef84083cef046d18a8
Bottleon <qncn93@gmail.com>
2022-11-11 15:44:04 +0900

    feature: add checklist object

--- src/main/java/com/example/demo/store/checklist/model/CheckList.java
+++ src/main/java/com/example/demo/store/checklist/model/CheckList.java
@@ -1,4 +1,36 @@
 package com.example.demo.store.checklist.model;
 
+import com.example.demo.common.converter.BooleanToYNConverter;
+import com.example.demo.hr.userstore.model.UserStore;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "checklist")
 public class CheckList {
+    @Id
+    @Column(name="checklist_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumns({
+            @JoinColumn(name="user_id"),
+            @JoinColumn(name="store_id")
+    })
+    private UserStore userStore;
+
+    @Column(name="regist_date",nullable = false,updatable = false)
+    @CreatedDate
+    private Date registDate;
+
+    @Column(name="content")
+    private String content;
+
+    @Convert(converter = BooleanToYNConverter.class)
+    private boolean checkStatus;
 }
