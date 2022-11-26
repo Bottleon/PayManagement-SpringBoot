4ee79c1a824290e20a6eee2c087d694e27f825a8
Bottleon <qncn93@gmail.com>
2022-11-26 23:00:07 +0900

    feature : redis - complete message stored in redis NoSQL and next itme, i need to save the token in redis

--- src/main/java/com/example/demo/base/sns/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.base.sns;
-
-
-import com.example.demo.hr.user.model.CertificationNumber;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<CertificationNumber,String> {
-}
--- src/main/java/com/example/demo/hr/user/model/CertificationNumber.java
+++ src/main/java/com/example/demo/hr/user/model/CertificationNumber.java
@@ -4,20 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
 
 import javax.persistence.*;
 
 @Data
-@Entity
-@Table(name="user_certification")
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CertificationNumber {
-    @Id
-    @Column(name = "user_id")
     private String id;
-
-    @Column(unique = true,nullable = false)
     private String certificationNumber;
 }
